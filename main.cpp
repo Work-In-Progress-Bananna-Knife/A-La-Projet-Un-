b403646539207884310b2026126655ff509188c3
Diego <dieveg2000@gmail.com>
2020-10-16 17:53:51 +0200

    wczytywanie plikow

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 #include "fun.cpp"
-
-
-
-int main(){
-
-    return 0;
+#include <dirent.h>
+#include <sys/types.h>
+using namespace std;
+int main(void) {
+   DIR *dr;
+   struct dirent *en;
+   dr = opendir("."); //open all directory
+   if (dr) {
+      while ((en = readdir(dr)) != NULL) {
+         cout<<" \n"<<en->d_name; //print all directory name
+      }
+      closedir(dr); //close all directory
+   }
+   return(0);
 }
