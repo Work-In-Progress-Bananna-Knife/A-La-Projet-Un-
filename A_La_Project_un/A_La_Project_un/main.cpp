0a61acab4fe9976c53ee3948a115f47eb8e48c68
OhhDoppio <zapartkrystian23@gmail.com>
2020-11-01 20:15:23 +0100

    I fell offff

---
+++ A_La_Project_un/A_La_Project_un/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  A_La_Project_un
+//
+//  Created by Krystian Zapart on 01/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
