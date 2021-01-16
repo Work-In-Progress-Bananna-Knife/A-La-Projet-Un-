8e820f872506186b6680a03a20b541254a57eab4
OhhDoppio <zapartkrystian23@gmail.com>
2021-01-16 20:05:28 +0100

    Zmiana podpisu

--- id.sh
+++ id.sh
@@ -1,5 +1,5 @@
 sed -i -e '$d' Data.gv
-echo "label = \"Repo v." >> Data.gv
+echo "label = \"#Commit nr." >> Data.gv
 git log --pretty="format:%h" -1 >> Data.gv
 echo "\"" >> Data.gv
 echo "}" >> Data.gv
