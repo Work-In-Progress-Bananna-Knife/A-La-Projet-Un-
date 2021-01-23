9041b96fa96eac68f2c16e35402dca755373e793
EcoXrusher <bartton21@gmail.com>
2021-01-24 00:18:07 +0100

    experimental directories to test new  functionalities as running program for diffrent one dir repo
    basic multi .cpp programs as printing simple text

---
+++ testSowy/janek.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Janusz{
+    public:
+        static void havingfan(){
+            std::cout<<"Janusz had a fan"<<std::endl<<"and  he had got fun with it\n";
+        }
+
+
+};
---
+++ testSowy/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "janek.cpp"
+
+using namespace std;
+
+
+int main(){
+
+    Janusz::havingfan();
+
+    return 0;
+}
